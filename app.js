29cd4a3e949b782d3fbf79a54544d471a936170d
Gajendrakm <gjendra@gmail.com>
2021-06-27 20:12:38 +0530

    newfilemodule

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const names = require('./4-names.js')
 const sayHi = require('./5-utils.js')
 
 const data = require('./6-alternative-flavor')
+const func1 = require('./7-mind-grenade')
+
 console.log(data)
 
 console.log("-----");
