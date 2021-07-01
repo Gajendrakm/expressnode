76847fee775ecfb3f6e86558d0141508dc18fc5c
Gajendrakm <gjendra@gmail.com>
2021-07-01 08:29:58 +0530

    read/write filePromise

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const { readFile } = require('fs')
+const { readFile,writeFile } = require('fs')
+const util = require('util')
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
 
 const getText = (path) => {
     return new Promise((resolve,reject) => {
@@ -14,13 +17,23 @@ const getText = (path) => {
 
 const start = async () =>{
     try{
-        const first = await getText('./content/first.txt')
-        const second = await getText('./content/second.txt')
+        //const first = await getText('./content/first.txt')
+        //const second = await getText('./content/second.txt')
+
+        const first = await readFilePromise('./content/first.txt')
+        const second = await readFilePromise('./content/second.txt')
+
+        await writeFilePromise('./content/result-mind-grenade.txt',
+        `THIS IS AWESOME :${first} ${second}`)
+
+
         console.log(first, second)
     } catch (error){
         console.log(error)
     }
 }
+
+start()
 // getText('./content/first.txt')
 // .then((result) => console.log(result))
 // .catch((err) => console.log(err))
